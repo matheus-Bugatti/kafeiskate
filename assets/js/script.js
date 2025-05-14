82f3d077622125d0f674fe0b29f6d3624c8dc5da
matheus-Bugatti <campos.n@aluno.ifsp.edu.br>
2025-05-14 14:10:29 -0300

    arrumando

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownToggle = document.querySelectorAll('[data-dropdown-toggle]');
+    dropdownToggle.forEach((toggle) => {
+        toggle.addEventListener('click', function () {
+            const parent = this.closest('.dropdown-item');
+            parent.classList.toggle('active');
+        })
+    })
+
+    document.addEventListener('click', function (e) {
+        dropdownToggle.forEach((toggle) => {
+            const parent = toggle.closest('.dropdown-item');
+            if (!parent.contains(e.target)) {
+                parent.classList.remove('active')
+            }
+        })
+    })
+})
+
+const searchToggle = document.getElementById('searchToggle');
+const searchOverlay = document.getElementById('searchOverlay');
+const closeSearch = document.getElementById('closeSearch');
+
+searchToggle.addEventListener('click', () => {
+    searchOverlay.style.display = 'flex';
+    setTimeout(() => {
+        searchOverlay.querySelector('.search-input').focus();
+    }, 100);
+});
+
+closeSearch.addEventListener('click', () => {
+    searchOverlay.style.display = 'none';
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        searchOverlay.style.display = 'none';
+    }
+});
+
+
+const imagens = document.querySelectorAll('.galeria img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+
+imagens.forEach((img) => {
+    img.addEventListener('click', () => {
+        lightboxImg.src = img.src;
+        lightbox.classList.add('show');
+    })
+});
+
+lightbox.addEventListener('click', () => {
+    lightbox.classList.remove('show');
+});
